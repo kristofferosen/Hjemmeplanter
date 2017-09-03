58f6fb713261cc6aa47e46f2886eedbff5b86586
kristofferosen <kristofferosen@gmail.com>
2017-09-03 14:00:46 -0400

    dispair

--- frontend/pageController.js
+++ frontend/pageController.js
@@ -18,57 +18,57 @@ App.controller('dashController', function ($scope, $http) {
         };
         $http(req).then(function(out){
 
-            console.log(out.data)
+            console.log(out.data);
 
-            $scope.8969837moist = [];
-            $scope.8969837temp = [];
+            $scope.node8969837moist = [];
+            $scope.node8969837temp = [];
 
-            $scope.1800741moist = [];
-            $scope.1800741temp = [];
+            $scope.node1800741moist = [];
+            $scope.node1800741temp = [];
 
-            $scope.1796642moist = [];
-            $scope.1796642temp = [];
+            $scope.node1796642moist = [];
+            $scope.node1796642temp = [];
 
-            $scope.14790735moist = [];
-            $scope.14790735temp = [];
+            $scope.node14790735moist = [];
+            $scope.node14790735temp = [];
 
-            $scope.8969661moist = [];
-            $scope.8969661temp = [];
+            $scope.node8969661moist = [];
+            $scope.node8969661temp = [];
 
             for(i = 0; i < out.data.length; i++){
 
                 if(out.data[i].chipcode == 8969837.000000){
 
-                   $scope.8969837moist.push(out.data[i].soilMoisture);
-                   $scope.8969837temp.push(out.data[i].soilTemperature);
+                   $scope.node8969837moist.push(out.data[i].soilMoisture);
+                   $scope.node8969837temp.push(out.data[i].soilTemperature);
 
                 }
 
                 if(out.data[i].chipcode == 1800741.000000){
 
-                   $scope.1800741moist.push(out.data[i].soilMoisture);
-                   $scope.1800741temp.push(out.data[i].soilTemperature);
+                   $scope.node1800741moist.push(out.data[i].soilMoisture);
+                   $scope.node1800741temp.push(out.data[i].soilTemperature);
 
                 }
 
                 if(out.data[i].chipcode == 1796642.000000){
 
-                   $scope.1796642moist.push(out.data[i].soilMoisture);
-                   $scope.1796642temp.push(out.data[i].soilTemperature);
+                   $scope.node1796642moist.push(out.data[i].soilMoisture);
+                   $scope.node1796642temp.push(out.data[i].soilTemperature);
 
                 }
 
                 if(out.data[i].chipcode == 14790735.000000){
 
-                   $scope.14790735moist.push(out.data[i].soilMoisture);
-                   $scope.14790735temp.push(out.data[i].soilTemperature);
+                   $scope.node14790735moist.push(out.data[i].soilMoisture);
+                   $scope.node14790735temp.push(out.data[i].soilTemperature);
 
                 }
 
                 if(out.data[i].chipcode == 8969661.000000){
 
-                   $scope.8969661moist.push(out.data[i].soilMoisture);
-                   $scope.8969661temp.push(out.data[i].soilTemperature);
+                   $scope.node8969661moist.push(out.data[i].soilMoisture);
+                   $scope.node8969661temp.push(out.data[i].soilTemperature);
 
                    date = new Date(out.data[i].time)
                    if(i%10 == 0){$scope.b.push(date.toLocaleString())}
@@ -93,20 +93,20 @@ App.controller('dashController', function ($scope, $http) {
             ];
 
 
-            $scope.14790735data = [
-                $scope.14790735moist,$scope.14790735temp
+            $scope.node14790735data = [
+                $scope.node14790735moist,$scope.node14790735temp
             ];
-            $scope.1800741data = [
-                $scope.1800741moist,$scope.1800741temp
+            $scope.node1800741data = [
+                $scope.node1800741moist,$scope.node1800741temp
             ];
-            $scope.8969837data = [
-                $scope.8969837moist,$scope.8969837temp
+            $scope.node8969837data = [
+                $scope.node8969837moist,$scope.node8969837temp
             ];
-            $scope.1796642data = [
-                $scope.1796642moist,$scope.1796642temp
+            $scope.node1796642data = [
+                $scope.node1796642moist,$scope.node1796642temp
             ];
-            $scope.8969661data = [
-                $scope.8969661moist,$scope.8969661temp
+            $scope.node8969661data = [
+                $scope.node8969661moist,$scope.node8969661temp
             ];
 
 
